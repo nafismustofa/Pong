5ac40498500ff4acdf1d5e50cf9e04f619e534c2
NFS101 <realnfs101@gmail.com>
2019-02-19 21:45:29 +0600

    Paddle Update

--- src/game.py
+++ src/game.py
@@ -3,25 +3,31 @@ import pygame
 class Game:
     #Variables
     __is_runnning = True
+    __is_paused = False
     __FPS = 60
-    __screen_size = (screen_width , screen_height) = (800 , 600)
+    __screen_size = (__screen_width , __screen_height) = (800 , 600)
     
     #Colors
     __color_black = (0 , 0 , 0)
     __color_white = (255 , 255 , 255)
+    __color_blue = (0 , 0 , 255)
+    __color_red = (255 , 0 , 0)
 
     #Paddles Variables
-    left_paddle_y = (screen_height // 2) - 50
-    right_paddle_y = (screen_height // 2) - 50
+    __left_paddle_y = (__screen_height // 2) - 50
+    __right_paddle_y = (__screen_height // 2) - 50
 
     #Objects
     __game_display = None
     __clock = pygame.time.Clock()
 
     #Draw Paddles Function
-    def __draw_paddles(self):
-        pygame.draw.rect(self.__game_display , self.__color_white , [10 , self.left_paddle_y , 20 , 100]) #Left Paddle
-        pygame.draw.rect(self.__game_display , self.__color_white , [(self.screen_width - 30) , self.left_paddle_y , 20 , 100]) #Left Paddle
+    def __paddles(self):
+        pygame.draw.rect(self.__game_display , self.__color_red , [10 , self.__left_paddle_y , 20 , 100]) #Left Paddle
+        pygame.draw.rect(self.__game_display , self.__color_blue , [(self.__screen_width - 30) , self.__right_paddle_y , 20 , 100]) #Right Paddle
+
+    #def __ball(self):
+        #pygame.draw.circle(self.__game_display , self.__color_white , )
 
     #Main Display Function
     def game(self):
@@ -38,12 +44,19 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.__is_runnning = False
+                #Input Event
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        if self.__is_paused == False:
+                            self.__is_paused = True
+                        else:
+                            self.__is_paused = False
 
             #Set Background Color
             self.__game_display.fill(self.__color_black)
 
-            #Draw Paddles
-            self.__draw_paddles()
+            #Initialize Paddles
+            self.__paddles()
 
             #Display Update
             pygame.display.update()
--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ from game import Game
 
 def main():
     Game().game()
+    
 if __name__ == "__main__":
     main()
